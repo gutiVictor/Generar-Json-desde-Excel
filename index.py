8fd5e628eef2508043c29e936ab93343f5542583
victor <gutierrezsanabria@hotmail.com>
2024-10-16 11:41:25 -0500

    integracion de creacion de json

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import pandas as pd
 import json
 
 
-excel_file = 'libro1.xlsx'  # Ruta del archivo Excel cambiar con el nombre 
+excel_file = 'A91024.xlsx'  # Ruta del archivo Excel cambiar con el nombre 
 
 # Leer la hoja del archivo Excel (asumiendo que la primera fila son encabezados)
 df = pd.read_excel(excel_file, header=None)
@@ -17,9 +17,11 @@ for index, row in df.iterrows():
         "descripcion": row[1],  # La segunda columna (row[1]) es la descripción
         "ref": row[2]           # La tercera columna (row[2]) es la referencia
     }
+    
+ 
 
 # Guardar el diccionario como archivo JSON
-with open('productos.json', 'w', encoding='utf-8') as json_file:
+with open('NewProductos.json', 'w', encoding='utf-8') as json_file:
     json.dump(productos_dict, json_file, ensure_ascii=False, indent=4)
 
 print("Archivo JSON generado exitosamente.")
